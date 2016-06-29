be20b14889bd98ba2fb0ad810e421b52ee88a588
Tyler Evans <tyler.r.evans@gmail.com>
2016-06-29 10:28:16 -0400

    Added space for testing.

--- samples/OAuth2ProtectedWebApi/Controllers/HomeController.cs
+++ samples/OAuth2ProtectedWebApi/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace OAuth2ProtectedWebApi.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
-			return View();
+			return View(); 
 		}
 	}
 }
